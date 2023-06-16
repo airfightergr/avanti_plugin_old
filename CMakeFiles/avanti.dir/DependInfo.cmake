5efe72e60ec1ce91e346eeb21cf93fe949999c4e
ILIAS TSELIOS <ilias@tselios.com>
2023-06-16 12:48:38 +0300

    New cmake setup

---
+++ CMakeFiles/avanti.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/5F839D945FF79065/X-Plane 12/Aircraft/X-Aviation/P180_Avanti_II/plugins/myavanti_plugin/avanti_plugin/src/avanti.cpp" "CMakeFiles/avanti.dir/avanti.cpp.o" "gcc" "CMakeFiles/avanti.dir/avanti.cpp.o.d"
+  "/mnt/5F839D945FF79065/X-Plane 12/Aircraft/X-Aviation/P180_Avanti_II/plugins/myavanti_plugin/avanti_plugin/src/units_conv.cpp" "CMakeFiles/avanti.dir/units_conv.cpp.o" "gcc" "CMakeFiles/avanti.dir/units_conv.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/CMakeFiles/avanti.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/avanti.dir/avanti.cpp.o"
+  "CMakeFiles/avanti.dir/avanti.cpp.o.d"
+  "CMakeFiles/avanti.dir/units_conv.cpp.o"
+  "CMakeFiles/avanti.dir/units_conv.cpp.o.d"
+  "avanti/lin_x64/avanti.xpl"
+  "avanti/lin_x64/avanti.xpl.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/avanti.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
